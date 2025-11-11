b4860dea9839f2ff9c3b9e2d845004728bbb057b
iThiemann <mail@thiemann.com>
2025-11-11 12:59:45 +0100

    change Terraform to v1.5.7

--- terraform-azure-ngnix/providers.tf
+++ terraform-azure-ngnix/providers.tf
@@ -2,7 +2,7 @@
 # Provider + Terraform settings
 ############################################################
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.5.7"
 
   required_providers {
     azurerm = {
